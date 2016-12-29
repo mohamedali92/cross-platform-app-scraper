a51615edfaafa18e4a0a2e6c3b70b03694d3ddf4
Mohamed Ali <mohamed.ali@alumni.ubc.ca>
2016-12-28 16:58:50 -0800

    have the scraper working for iOS apps

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
+#from bs4 import BeautifulSoup
+import json
+import requests
+import sys
+#import urllib2
+
 
 class App:
 
-    def __init__(self, id, name, stars, ratings, last_updated):
-
+    def __init__(self, id, name, stars, ratings, lastUpdated):
         self.id = id
         self.name = name
         self.stars = stars
         self.ratings = ratings
-        self.last_updated = last_updated
-        
+        self.lastUpdated = lastUpdated
+
+    def __str__(self):
+        return str(self.id) +"\n"+str(self.name)+"\n"+str(self.stars)+"\n"+str(self.ratings)+"\n"+str(self.lastUpdated)
+
+class AttributeScraper:
+    def __init__(self, baseUrl, platform):
+        self.baseUrl = baseUrl
+        self.platform = platform
+
+    def scrape(self):
+        try:
+            r = requests.get(self.baseUrl)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print (err)
+            sys.exit(1)
+
+        if self.platform == 'ios':
+            self.scrapeIos(r)
+        else:
+            self.scrapeAndroid(r)
+
+    def scrapeIos(self, response):
+        data = response.json()['results'][0]
+        id = data['trackId']
+        name = data['trackCensoredName']
+        stars = data['averageUserRating']
+        ratings = data['userRatingCount']
+        lastUpdated = data['currentVersionReleaseDate']
+
+        print(App(id, name, stars, ratings, lastUpdated))
+
+
+
+
+    def scrapeAndroid(self, response):
+        print ("hello")
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from app import App
-import sys
+from app import AttributeScraper
 import argparse
+
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--android', required=True, type=str, help='Android app id')
 parser.add_argument('-i', '--ios', required=True, type=str, help='ios app id' )
-args = parser.parse_args()
+args =vars(parser.parse_args())
+
+iosUrl = 'https://itunes.apple.com/lookup?id=' + args['ios']
+iosXPaths = {'id':'trackId' ,'name': 'trackCensoredName', 'stars': 'averageUserRating', 'ratings': 'userRatingCount', 'lastUpdated': 'currentVersionReleaseDate'}
+androidUrl = 'https://play.google.com/store/apps/details?id=' + args['android']
+
+AttributeScraper(iosUrl, "ios").scrape()
+
+
+# print(iosUrl)
 
 
 
-def scrape(base_url, xpaths):
-    print("scraper")
+# print(androidUrl)
+
